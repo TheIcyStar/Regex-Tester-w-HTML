66f38a4aa8f6276899a4d06194c5ff1f7231edff
TheIcyStar <TheIcyStar@users.noreply.github.com>
2016-11-06 14:40:17 -0500

    Updated AssemblyInfo.cs with the proper project name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("temp2")]
+[assembly: AssemblyTitle("RegexTester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("temp2")]
+[assembly: AssemblyProduct("RegexTester")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
